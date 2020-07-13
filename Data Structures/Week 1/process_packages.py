a94075b23f936ca03eba3d876dc21a40531eeee5
jeyansaran620 <46715243+jeyansaran620@users.noreply.github.com>
2020-07-13 20:07:17 +0530

    adding network simulation

---
+++ Data Structures/Week 1/process_packages.py
@@ -0,0 +1,62 @@
+# python3
+
+from collections import namedtuple
+
+Request = namedtuple("Request", ["arrived_at", "time_to_process"])
+Response = namedtuple("Response", ["was_dropped", "started_at"])
+
+
+class Buffer:
+    def __init__(self, size):
+        self.size = size
+        self.finish_time = []
+
+    def process(self, request):
+        
+        if len(self.finish_time) == 0:
+            self.finish_time.append(request[0]+request[1])
+            return Response(False, request[0])
+        
+        time = self.finish_time[-1]
+        
+        if self.finish_time[0] <= request[0]:
+            del self.finish_time[0]
+            
+        if len(self.finish_time) == 0:
+            self.finish_time.append(request[0]+request[1])
+            return Response(False, request[0])
+        
+        if len(self.finish_time) < self.size:
+
+            self.finish_time.append(time + request[1])
+            return Response(False, time)
+        
+        else:
+            return Response(True, None)
+        
+            
+
+
+def process_requests(requests, buffer):
+    responses = []
+    for request in requests:
+        responses.append(buffer.process(request))
+    return responses
+
+
+def main():
+    buffer_size, n_requests = map(int, input().split())
+    requests = []
+    for _ in range(n_requests):
+        arrived_at, time_to_process = map(int, input().split())
+        requests.append(Request(arrived_at, time_to_process))
+
+    buffer = Buffer(buffer_size)
+    responses = process_requests(requests, buffer)
+
+    for response in responses:
+        print(response.started_at if not response.was_dropped else -1)
+
+
+if __name__ == "__main__":
+    main()
